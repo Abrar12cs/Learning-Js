6703128972aed64be86ceb633d44b90c07fbb801
Abrar12cs <135686768+Abrar12cs@users.noreply.github.com>
2024-08-23 05:57:07 +0000

    dataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,27 @@
+"use strict" // treat all js code as a newer version
+
+// alert(3+3) we are using nodejs, not browser.
+
+/** 
+ for documentation use 
+ 1. mdn
+ 2. https://tc39.es/ecma262/2023/multipage/
+*/
+
+// number => 2 to power 53
+// bigInt
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => mean value is not assigned
+// symbol => unique 
+
+
+// object
+
+console.log(typeof "Abrar") // string
+console.log(typeof undefined) // undefined
+console.log(typeof null) // object 
+
+
+
